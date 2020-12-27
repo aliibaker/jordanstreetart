58053f21c7bf2ea115a822828008a9fdb2481753
bakes <alibbaker@outlook.com>
2020-12-28 01:02:12 +0200

    temp fixes

--- sql/credits_data.sql
+++ sql/credits_data.sql
@@ -1,2 +1,1 @@
-INSERT INTO credits(graffiti_id, artist_id) VALUES(24, 0); -- credited to Suhaib Attar
-INSERT INTO credits(graffiti_id, artist_id) VALUES(24, 1); -- credited to Suhaib Attar
+INSERT INTO credits(graffiti_id, artist_id) VALUES(24, 0); 
